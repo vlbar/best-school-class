7d03dd86ab3290766c459a5152e03212a157e617
Vladislav Baranov <vladislavbaranov290301@gmail.com>
2021-05-10 15:41:23 +0300

    Added basic Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const path = require('path');
+const isDev = process.env.NODE_ENV !== 'production';
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[fullhash].${ext}`;
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    output: { 
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'build')
+    },
+    devServer: {
+        port: 4200
+    },
+    resolve: {
+        extensions: ['.js', '.json'],
+    },
+    optimization: {
+        minimize: !isDev,
+        minimizer: [
+            new TerserWebpackPlugin(),
+            new OptimizeCssPlugin()
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+                template: './index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|less)$/,
+                use: [MiniCssExtractPlugin.loader,'css-loader', 'less-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|eot)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+} 
