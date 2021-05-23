ab08d5c5e3b0271c8788bcbf2919a34cf61cd617
Pylenkov Roman <solodov1994roma@gmail.com>
2021-05-24 01:27:01 +0300

    Added configuration for intercepting 401 responces and try refresh token

--- src/App.jsx
+++ src/App.jsx
@@ -12,27 +12,13 @@ import Groups from "./pages/Groups";
 import Login from "./pages/Login";
 
 import NotFound from "./pages/NotFound";
-import ErrorHandler from "./components/auth/authorize/ErrorHandler";
 import axios from "axios";
-import Register from "./pages/Register";
-import { store } from "./redux/rootReducer";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import PublicRoute from "./components/routing/PublicRoute";
 import { HELPER, STUDENT, TEACHER } from "./redux/state/stateActions";
+import configureAxios from "./config/axios-config";
 
-const BASE_PATH = "https://dss-course-work.herokuapp.com/api/v1";
-axios.defaults.baseURL = BASE_PATH;
-axios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    return Promise.reject(
-      ErrorHandler(store, error)
-    );
-  }
-);
-axios.defaults.headers.common['Authorization'] = `Bearer ${JSON.parse(localStorage.getItem("auth"))?.user.token }`;
+configureAxios(axios);
 
 function App() {
   return (
@@ -42,10 +28,18 @@ function App() {
         <Route path={"/"} exact component={Index} />
         <PublicRoute path={"/login"} component={Login} />
         <PrivateRoute path={"/home"} component={Workspace} />
-        <PrivateRoute path={"/shedule"} component={Shedule} allowedStates={[STUDENT, TEACHER]}/>
-        <PrivateRoute path={"/answers"} component={Answers}/>
-        <PrivateRoute path={"/courses"} component={Courses} allowedStates={[HELPER, TEACHER]}/>
-        <Route path={"/groups"} component={Groups} />
+        <PrivateRoute
+          path={"/shedule"}
+          component={Shedule}
+          allowedStates={[STUDENT, TEACHER]}
+        />
+        <PrivateRoute path={"/answers"} component={Answers} />
+        <PrivateRoute
+          path={"/courses"}
+          component={Courses}
+          allowedStates={[HELPER, TEACHER]}
+        />
+        <PrivateRoute path={"/groups"} component={Groups} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
