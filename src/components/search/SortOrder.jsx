29bf387732ff0312fa731dff655ee6e93002372d
Pylenkov Roman <solodov1994roma@gmail.com>
2021-06-25 15:37:39 +0300

    Added search shit

---
+++ src/components/search/SortOrder.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+import { Dropdown } from "react-bootstrap";
+
+function SortOrder({ orders, variant, initialOrder, onSelect, ...props }) {
+  const [currentOrder, setCurrentOrder] = useState(initialOrder ?? Object.keys(orders)[0]);
+
+  function handleSelect(eventKey) {
+    onSelect(eventKey);
+    setCurrentOrder(eventKey)
+  }
+
+  return (
+    <Dropdown {...props}>
+      <Dropdown.Toggle variant={variant ?? "outline-ligth"} className="w-100">
+        {orders[currentOrder]}
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        {Object.keys(orders).map((order, index) => {
+          return (
+            <Dropdown.Item
+              onSelect={handleSelect}
+              eventKey={order}
+              key={index}
+            >
+              {orders[order]}
+            </Dropdown.Item>
+          );
+        })}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default SortOrder;
