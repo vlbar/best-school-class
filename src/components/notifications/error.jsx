9411311122e6b201d24c5ef0c372c1c166b788a0
Pylenkov Roman <solodov1994roma@gmail.com>
2021-06-25 15:38:31 +0300

    Added base notification setting

---
+++ src/components/notifications/error.jsx
@@ -0,0 +1,12 @@
+const errorNotification = {
+    title: "Произошла ошибка",
+    message: "Перезагрузите страницу, если ошибка повторится то попробуйте позже",
+    type: "danger",
+    insert: "top",
+    container: "top-right",
+    animationIn: ["animate__animated animate__fadeIn"],
+    animationOut: ["animate__animated animate__fadeOut"],
+    showIcon: true
+  };
+
+export default errorNotification;
