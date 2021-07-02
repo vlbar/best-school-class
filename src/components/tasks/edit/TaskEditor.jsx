6fcd64fd316803c91e8ce5de36851e5827b3618b
Vladislav Baranov <62684874+vlbar@users.noreply.github.com>
2021-07-02 14:34:36 +0300

    Added task fetching and task details reducer

--- src/components/tasks/edit/TaskEditor.jsx
+++ src/components/tasks/edit/TaskEditor.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useReducer, useRef } from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import { sortableContainer, sortableElement, arrayMove } from 'react-sortable-hoc'
 import { TaskQuestion } from './TaskQuestion'
+import { addErrorNotification } from '../../notifications/notifications'
+import ProcessBar from '../../process-bar/ProcessBar'
+import axios from 'axios'
 import './TaskEditor.less'
 
 const MINUTES = 'MINUTES'
@@ -24,15 +27,44 @@ const getTimeName = (type, num) => {
     }
 }
 
+//reducer
+const TASK = 'TASK'
+const TASK_NAME = 'NAME'
+const TASK_DESC = 'DESCRIPTION'
+const TASK_MAX_SCORE = 'MAX_SCORE'
+const TASK_DURATION = 'DURATION'
+
+const taskReducer = (state, action) => {
+    switch (action.type) {
+        case TASK:
+            return { ...action.payload }
+        case TASK_NAME:
+            return { ...state, name: action.payload }
+        case TASK_DESC:
+            return { ...state, description: action.payload }
+        case TASK_MAX_SCORE:
+            return { ...state, maxScore: action.payload }
+        case TASK_DURATION:
+            return { ...state, duration: action.payload }
+        default:
+            return state
+    }
+}
+
 // sortable hoc
 const SortableContainer = sortableContainer(({children}) => <div>{children}</div>)
 const SortableItem = sortableElement(({question, questionToEditHadnle, questionToEdit}) => (
     <TaskQuestion question={question} questionToEdit={questionToEdit} questionToEditHadnle={questionToEditHadnle}/>
 ))
-  
+
+const tasksBaseUrl = '/tasks'
+
+async function fetchTaskDetails(taskId) {
+    return axios.get(`${tasksBaseUrl}/${taskId}`)
+}
 
 export const TaskEditor = ({taskId}) => {
-    const [duration, setDuration] = useState('')
+    const [isTaskFetching, setIsTaskFetching] = useState(true)
     const [questions, setQuestions] = useState([
         {
             id: 1
@@ -50,16 +82,48 @@ export const TaskEditor = ({taskId}) => {
         setQuestions(arrayMove(questions, oldIndex, newIndex))
     }
 
+    useEffect(() => {
+        fetchTask()
+    }, [])
+
+    const [taskDetails, taskDispatch] = useReducer(taskReducer, {})
+
+    const setTask = (task) => taskDispatch({ type: TASK, payload: task })
+    const setName = (name) => taskDispatch({ type: TASK_NAME, payload: name })
+    const setDescription = (description) => taskDispatch({ type: TASK_DESC, payload: description })
+    const setMaxScore = (maxScore) => taskDispatch({ type: TASK_MAX_SCORE, payload: maxScore })
+    const setDuration = (duration) => taskDispatch({ type: TASK_DURATION, payload: duration })
+
+    const fetchTask = (state) => {
+        setIsTaskFetching(true)
+        fetchTaskDetails(taskId)
+            .then(res => {
+                let fetchedData = res.data
+                setTask(fetchedData)
+                setIsTaskFetching(false)
+            })
+            .catch(error => 
+                addErrorNotification('Не удалось загрузить информацию о задании. \n' + (error?.response?.data ? error.response.data.message : error))
+            )
+    }
+
     return (
         <>
             <Container>
                 <h4 className='mt-3'>Задание</h4>
+                <ProcessBar active={isTaskFetching} height='.18Rem' className='mb-2'/>
                 <Form.Group as={Row}>
                     <Form.Label column sm={2}>
                         Название
                     </Form.Label>
                     <Col sm={10}>
-                        <Form.Control type='text' placeholder='Введите название задания...' />
+                        <Form.Control 
+                            type='text' 
+                            placeholder='Введите название задания...'
+                            disabled={isTaskFetching}
+                            value={taskDetails?.name || ''}
+                            onChange={(e) => setName(e.target.value)}
+                        />
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row}>
@@ -67,7 +131,15 @@ export const TaskEditor = ({taskId}) => {
                         Описание задания
                     </Form.Label>
                     <Col sm={10}>
-                        <Form.Control as='textarea' placeholder='Введите описание задания...' rows={3} style={{maxHeight: '86px', minHeight: '40px'}}/>
+                        <Form.Control 
+                            as='textarea' 
+                            placeholder='Введите описание задания...' 
+                            rows={3}
+                            disabled={isTaskFetching}
+                            value={taskDetails?.description || ''}
+                            onChange={(e) => setDescription(e.target.value)}
+                            style={{maxHeight: '86px', minHeight: '40px'}}
+                        />
                     </Col>
                 </Form.Group>
                 <Form.Group as={Row}>
@@ -75,7 +147,13 @@ export const TaskEditor = ({taskId}) => {
                         Максимальная оценка
                     </Form.Label>
                     <Col sm={10}>
-                        <Form.Control type='number' min={1} defaultValue='100' placeholder='Введите максимальный балл...' />
+                        <Form.Control 
+                            type='number' 
+                            min={1} 
+                            disabled={isTaskFetching}
+                            value={taskDetails?.maxScore || ''}
+                            onChange={(e) => setMaxScore(e.target.value)}
+                            placeholder='Введите максимальный балл...' />
                         <Form.Text className="text-muted">
                             К этому числу будут переводится количество баллов, полученные учеником
                         </Form.Text>
@@ -87,11 +165,17 @@ export const TaskEditor = ({taskId}) => {
                     </Form.Label>
                     <Col sm={10}>
                         <div className='task-duration'>
-                            <Form.Control type='number' min={1} value={duration} onChange={(e) => setDuration(e.target.value)}/>
+                            <Form.Control 
+                                type='number' 
+                                min={1}
+                                disabled={isTaskFetching}
+                                value={taskDetails?.duration || ''}
+                                onChange={(e) => setDuration(e.target.value)}
+                            />
                             <Form.Control as="select">
-                                <option>{getTimeName(MINUTES, duration)}</option>
-                                <option>{getTimeName(HOURS, duration)}</option>
-                                <option>{getTimeName(DAYS, duration)}</option>
+                                <option>{getTimeName(MINUTES, taskDetails?.duration || '')}</option>
+                                <option>{getTimeName(HOURS, taskDetails?.duration || '')}</option>
+                                <option>{getTimeName(DAYS, taskDetails?.duration || '')}</option>
                             </Form.Control>
                         </div>
                     </Col>
