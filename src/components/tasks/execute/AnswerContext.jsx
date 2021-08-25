b6bcb05df919a25362fd5d915f22c4353e0445e4
Vladislav Baranov <62684874+vlbar@users.noreply.github.com>
2021-08-25 13:13:17 +0300

    Removed modal confirmation of task start

--- src/components/tasks/execute/AnswerContext.jsx
+++ src/components/tasks/execute/AnswerContext.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
+import { Button } from 'react-bootstrap'
 
 import { addErrorNotification } from '../../notifications/notifications'
-import ConfirmStartTask from './ConfirmStartTask'
 import ProcessBar from '../../process-bar/ProcessBar'
 import { tasksBaseUrl } from '../../../pages/TaskAnswer'
 import QuestionAnswerList from './question-answer/QuestionAnswerList'
@@ -20,6 +20,7 @@ async function createAnswer(taskId, role) {
 const AnswerContext = ({ task, role = 'student' }) => {
     const [isFetching, setIsFetching] = useState(false)
     const [answers, setAnswers] = useState(undefined)
+    const [selectedAnswerTry, setSelectedAnswerTry] = useState(undefined)
     const [isConfirmed, setIsConfirmed] = useState(undefined)
 
     useEffect(() => {
@@ -33,6 +34,7 @@ const AnswerContext = ({ task, role = 'student' }) => {
             .then(res => {
                 let fetchedData = res.data
                 setAnswers(fetchedData.items)
+                setSelectedAnswerTry(fetchedData.items[0])
                 setIsConfirmed(fetchedData.items.length > 0)
             })
             .catch(error => addErrorNotification('Не удалось загрузить ответ на задание. \n' + error))
@@ -47,6 +49,7 @@ const AnswerContext = ({ task, role = 'student' }) => {
             .then(res => {
                 let fetchedData = res.data
                 setAnswers([...answers, fetchedData])
+                setSelectedAnswerTry(fetchedData)
             })
             .catch(error => addErrorNotification('Не удалось создать ответ на задание. \n' + error))
             .finally(() => setIsFetching(false))
@@ -54,10 +57,21 @@ const AnswerContext = ({ task, role = 'student' }) => {
 
     return (
         <>
-            {isFetching && <ProcessBar height='.18Rem' className='mt-2' />}
             <h5 className='mb-4'>Вопросы:</h5>
-            {isConfirmed === false && <ConfirmStartTask task={task} onConfirm={() => createTaskAnswer()} />}
-            {isConfirmed && answers[0] && (<QuestionAnswerList answerId={answers[0].id} />)}
+            {isConfirmed === false && (
+                <div className='text-center'>
+                    <div className='mx-auto mb-3'>
+                        <b>Вопросы еще не получены, так как выполнение задания еще не начато.</b>
+                        <br />
+                        Вы можете ознакомится с условиями задания и начать его выполнение.
+                    </div>
+                    <Button variant='outline-primary' type='submit' onClick={() => createTaskAnswer()}>
+                        Начать выполнение
+                    </Button>
+                </div>
+            )}
+            {isFetching && <ProcessBar height='.18Rem' className='mt-2' />}
+            {isConfirmed && selectedAnswerTry && (<QuestionAnswerList answerId={selectedAnswerTry.id} />)}
         </>
     )
 }
